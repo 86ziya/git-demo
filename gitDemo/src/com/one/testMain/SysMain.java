eaf4c1c715072a81de824dd309523532ca8a7476
86ziya <2326892719@qq.com>
2022-05-04 22:44:16 +0800

    冲突测试， test的修改

--- gitDemo/src/com/one/testMain/SysMain.java
+++ gitDemo/src/com/one/testMain/SysMain.java
@@ -3,5 +3,7 @@ package com.one.testMain;
 public class SysMain {
     public static void main(String[] args) {
         System.out.println("可行的main入口类");
+
+        System.out.println("这是test分支的修改");
     }
 }
