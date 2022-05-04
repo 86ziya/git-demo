dd949c6906050cb44521c29f9f8fa8403032ba46
86ziya <2326892719@qq.com>
2022-05-04 22:45:30 +0800

    冲突测试， main 主干的修改

--- gitDemo/src/com/one/testMain/SysMain.java
+++ gitDemo/src/com/one/testMain/SysMain.java
@@ -3,5 +3,7 @@ package com.one.testMain;
 public class SysMain {
     public static void main(String[] args) {
         System.out.println("可行的main入口类");
+
+        System.out.println("这是 main 主干的修改");
     }
 }
